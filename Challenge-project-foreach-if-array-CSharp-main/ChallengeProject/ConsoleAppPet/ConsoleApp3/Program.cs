103d71cca6b1b1caaf95753ce9ba402a83a9d1e9
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2024-10-03 19:53:16 -0300

    Update
    Atualizações Novos Modulos

---
+++ Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/ConsoleAppPet/ConsoleApp3/Program.cs
@@ -0,0 +1,578 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // #1 the ourAnimals array will store the following: 
+            string animalSpecies = "";
+            string animalID = "";
+            string animalAge = "";
+            string animalPhysicalDescription = "";
+            string animalPersonalityDescription = "";
+            string animalNickname = "";
+            string suggestedDonation = "";
+
+            // #2 variables that support data entry
+            int maxPets = 8;
+            string? readResult;
+            string menuSelection = "";
+
+            // #3 array used to store runtime data, there is no persisted data
+            string[,] ourAnimals = new string[maxPets, 6];
+
+
+            // #4 create sample data ourAnimals array entries
+            for (int i = 0; i < maxPets; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        animalSpecies = "dog";
+                        animalID = "d1";
+                        animalAge = "2";
+                        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
+                        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                        animalNickname = "lola";
+                        break;
+
+                    case 1:
+                        animalSpecies = "dog";
+                        animalID = "d2";
+                        animalAge = "9";
+                        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+                        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+                        animalNickname = "gus";
+                        break;
+
+                    // case 2: deleted for brevity
+                    // case 3: deleted for brevity
+
+                    default:
+                        animalSpecies = "";
+                        animalID = "";
+                        animalAge = "";
+                        animalPhysicalDescription = "";
+                        animalPersonalityDescription = "";
+                        animalNickname = "";
+                        break;
+                }
+                ourAnimals[i, 0] = "ID #: " + animalID;
+                ourAnimals[i, 1] = "Species: " + animalSpecies;
+                ourAnimals[i, 2] = "Age: " + animalAge;
+                ourAnimals[i, 3] = "Nickname: " + animalNickname;
+                ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+                ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+            }
+
+
+            //criar metodos///////////////////////////////////////////////////////////////////
+
+            void DisplayRandomNumbers()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write($"{random.Next(1, 100)} ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            using System;
+
+            int[] times = { 800, 1200, 1600, 2000 };
+            int diff = 0;
+
+            Console.WriteLine("Enter current GMT");
+            int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Current Medicine Schedule:");
+
+            /* Format and display medicine times */
+            foreach (int val in times)
+            {
+                string time = val.ToString();
+                int len = time.Length;
+
+                if (len >= 3)
+                {
+                    time = time.Insert(len - 2, ":");
+                }
+                else if (len == 2)
+                {
+                    time = time.Insert(0, "0:");
+                }
+                else
+                {
+                    time = time.Insert(0, "0:0");
+                }
+
+                Console.Write($"{time} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter new GMT");
+            int newGMT = Convert.ToInt32(Console.ReadLine());
+
+            if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+            {
+                Console.WriteLine("Invalid GMT");
+            }
+            else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+            {
+                diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+
+                /* Adjust the times by adding the difference, keeping the value within 24 hours */
+                for (int i = 0; i < times.Length; i++)
+                {
+                    times[i] = ((times[i] + diff)) % 2400;
+                }
+            }
+            else
+            {
+                diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+                /* Adjust the times by adding the difference, keeping the value within 24 hours */
+                for (int i = 0; i < times.Length; i++)
+                {
+                    times[i] = ((times[i] + diff)) % 2400;
+                }
+            }
+
+            Console.WriteLine("New Medicine Schedule:");
+
+            /* Format and display medicine times */
+            foreach (int val in times)
+            {
+                string time = val.ToString();
+                int len = time.Length;
+
+                if (len >= 3)
+                {
+                    time = time.Insert(len - 2, ":");
+                }
+                else if (len == 2)
+                {
+                    time = time.Insert(0, "0:");
+                }
+                else
+                {
+                    time = time.Insert(0, "0:0");
+                }
+
+                Console.Write($"{time} ");
+            }
+
+            Console.WriteLine();
+
+
+            void DisplayTimes()
+            {
+                /* Format and display medicine times */
+                foreach (int val in times)
+                {
+                    string time = val.ToString();
+                    int len = time.Length;
+
+                    if (len >= 3)
+                    {
+                        time = time.Insert(len - 2, ":");
+                    }
+                    else if (len == 2)
+                    {
+                        time = time.Insert(0, "0:");
+                    }
+                    else
+                    {
+                        time = time.Insert(0, "0:0");
+                    }
+
+                    Console.Write($"{time} ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            void AdjustTimes()
+            {
+                /* Adjust the times by adding the difference, keeping the value within 24 hours */
+                for (int i = 0; i < times.Length; i++)
+                {
+                    times[i] = ((times[i] + diff)) % 2400;
+                }
+            }
+
+            ///////////Verific ip //////////////////////
+
+            string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
+            string[] address;
+            bool validLength = false;
+            bool validZeroes = false;
+            bool validRange = false;
+
+            foreach (string ip in ipv4Input)
+            {
+                address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+                ValidateLength();
+                ValidateZeroes();
+                ValidateRange();
+
+                if (validLength && validZeroes && validRange)
+                {
+                    Console.WriteLine($"{ip} is a valid IPv4 address");
+                }
+                else
+                {
+                    Console.WriteLine($"{ip} is an invalid IPv4 address");
+                }
+            }
+
+            void ValidateLength()
+            {
+                validLength = address.Length == 4;
+            };
+
+            void ValidateZeroes()
+            {
+                foreach (string number in address)
+                {
+                    if (number.Length > 1 && number.StartsWith("0"))
+                    {
+                        validZeroes = false;
+                        return;
+                    }
+                }
+
+                validZeroes = true;
+            }
+
+            void ValidateRange()
+            {
+                foreach (string number in address)
+                {
+                    int value = int.Parse(number);
+                    if (value < 0 || value > 255)
+                    {
+                        validRange = false;
+                        return;
+                    }
+                }
+                validRange = true;
+            }
+
+            ///////////////////////////////////método reutilizável/////////////////////////////////////////
+
+            //////////////////////////////////Usar parâmetros em métodos///////////////////////////////////
+
+            int[] schedule = { 800, 1200, 1600, 2000 };
+
+            void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+            {
+                int diff = 0;
+                if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+                {
+                    Console.WriteLine("Invalid GMT");
+                }
+                else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+                {
+                    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+                }
+                else
+                {
+                    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+                }
+                for (int i = 0; i < times.Length; i++)
+                {
+                    int newTime = ((times[i] + diff)) % 2400;
+                    Console.WriteLine($"{times[i]} -> {newTime}");
+                }
+            }
+            DisplayAdjustedTimes(schedule, 6, -6);
+
+            string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+            DisplayStudents(students);
+            DisplayStudents(new string[] { "Robert", "Vanya" });
+
+            void DisplayStudents(string[] students)
+            {
+                foreach (string student in students)
+                {
+                    Console.Write($"{student}, ");
+                }
+                Console.WriteLine();
+            }
+
+            PrintCircleArea(12);
+
+            void PrintCircleArea(int radius)
+            {
+                double pi = 3.14159;
+                double area = pi * (radius * radius);
+                Console.WriteLine($"Area = {area}");
+            }
+
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            PrintArray(array);
+            Clear(array);
+            PrintArray(array);
+
+            void PrintArray(int[] array)
+            {
+                foreach (int a in array)
+                {
+                    Console.Write($"{a} ");
+                }
+                Console.WriteLine();
+            }
+
+            void Clear(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = 0;
+                }
+            }
+
+            string status = "Healthy";
+
+            Console.WriteLine($"Start: {status}");
+            SetHealth(status, false);
+            Console.WriteLine($"End: {status}");
+
+            void SetHealth(string status, bool isHealthy)
+            {
+                status = (isHealthy ? "Healthy" : "Unhealthy");
+                Console.WriteLine($"Middle: {status}");
+            }
+
+            //Criar um aplicativo RSVP//
+
+            string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+            string[] rsvps = new string[10];
+            int count = 0;
+
+            void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+            {
+                if (inviteOnly)
+                {
+                    // search guestList before adding rsvp
+                }
+
+                rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+                count++;
+            }
+
+            void ShowRSVPs()
+            {
+                Console.WriteLine("\nTotal RSVPs:");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine(rsvps[i]);
+                }
+            }
+
+            ///criançao de email
+
+            string[,] corporate =
+                {
+        {"Robert", "Bavin"}, {"Simon", "Bright"},
+        {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+        {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+            string[,] external =
+            {
+        {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+        {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+    };
+
+            string externalDomain = "hayworth.com";
+
+            for (int i = 0; i < corporate.GetLength(0); i++)
+            {
+                DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]);
+            }
+
+            for (int i = 0; i < external.GetLength(0); i++)
+            {
+                DisplayEmail(first: external[i, 0], last: external[i, 1], domain: externalDomain);
+            }
+
+            void DisplayEmail(string first, string last, string domain = "contoso.com")
+            {
+                string email = first.Substring(0, 2) + last;
+                email = email.ToLower();
+                Console.WriteLine($"{email}@{domain}");
+            }
+
+            //////calcular o preço total de compra/////
+
+            double total = 0;
+            double minimumSpend = 30.00;
+
+            double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+            double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+            Console.WriteLine($"Total: ${total}");
+            double total = 0;
+            double minimumSpend = 30.00;
+
+            double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+            double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                total += GetDiscountedPrice(i);
+            }
+
+            total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+            Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+            double GetDiscountedPrice(int itemIndex)
+            {
+                return items[itemIndex] * (1 - discounts[itemIndex]);
+            }
+
+            bool TotalMeetsMinimum()
+            {
+                return total >= minimumSpend;
+            }
+
+            string FormatDecimal(double input)
+            {
+                return input.ToString().Substring(0, 5);
+            }
+
+
+            double usd = 23.73;
+            int vnd = UsdToVnd(usd);
+
+            Console.WriteLine($"${usd} USD = ${vnd} VND");
+            Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+            int UsdToVnd(double usd)
+            {
+                int rate = 23500;
+                return (int)(rate * usd);
+            }
+
+            double VndToUsd(int vnd)
+            {
+                double rate = 23500;
+                return vnd / rate;
+            }
+
+
+            int target = 30;
+            int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+            int[,] result = TwoCoins(coins, target);
+
+            if (result.Length == 0)
+            {
+                Console.WriteLine("No two coins make change");
+            }
+            else
+            {
+                Console.WriteLine("Change found at positions:");
+                for (int i = 0; i < result.GetLength(0); i++)
+                {
+                    if (result[i, 0] == -1)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+                }
+            }
+
+            int[,] TwoCoins(int[] coins, int target)
+            {
+                int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+                int count = 0;
+
+                for (int curr = 0; curr < coins.Length; curr++)
+                {
+                    for (int next = curr + 1; next < coins.Length; next++)
+                    {
+                        if (coins[curr] + coins[next] == target)
+                        {
+                            result[count, 0] = curr;
+                            result[count, 1] = next;
+                            count++;
+                        }
+                        if (count == result.GetLength(0))
+                        {
+                            return result;
+                        }
+                    }
+                }
+                return (count == 0) ? new int[0, 0] : result;
+            }
+
+            ///jogo///
+
+            Random random = new Random();
+
+            Console.WriteLine("Would you like to play? (Y/N)");
+            if (ShouldPlay())
+            {
+                PlayGame();
+            }
+
+            bool ShouldPlay()
+            {
+                string response = Console.ReadLine();
+                return response.ToLower().Equals("y");
+            }
+
+            void PlayGame()
+            {
+                var play = true;
+
+                while (play)
+                {
+                    var target = GetTarget();
+                    var roll = RollDice();
+
+                    Console.WriteLine($"Roll a number greater than {target} to win!");
+                    Console.WriteLine($"You rolled a {roll}");
+                    Console.WriteLine(WinOrLose(roll, target));
+                    Console.WriteLine("\nPlay again? (Y/N)");
+
+                    play = ShouldPlay();
+                }
+            }
+
+            int GetTarget()
+            {
+                return random.Next(1, 6);
+            }
+
+            int RollDice()
+            {
+                return random.Next(1, 7);
+            }
+
+            string WinOrLose(int roll, int target)
+            {
+                if (roll > target)
+                {
+                    return "You win!";
+                }
+                return "You lose!";
+            }
+
+
+        }
+    }
+}
